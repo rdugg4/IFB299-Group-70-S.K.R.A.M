7008126cf215cb4ea66b7869bea1fb0bb5692d8c
rdugg4 <rdugg4@users.noreply.github.com>
2018-09-11 10:35:52 +1000

    Index page update and no longer test app

--- SKRAM70/SKRAM70/urls.py
+++ SKRAM70/SKRAM70/urls.py
@@ -17,5 +17,5 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('testApp/', include('testApp.urls')),
+    path('CRC/', include('testApp.urls')),
 ]
