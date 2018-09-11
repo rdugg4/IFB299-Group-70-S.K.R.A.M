f867be02f03d84ef94580b1ec6abb764edf1f27d
rdugg4 <rdugg4@users.noreply.github.com>
2018-09-11 10:20:46 +1000

    Search Results with hard coded search

--- SKRAM70/testApp/urls.py
+++ SKRAM70/testApp/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:car_id>/', views.detail, name='detail'),
+    path('searchResults', views.results, name='something')
 ]
--- SKRAM70/testApp/views.py
+++ SKRAM70/testApp/views.py
@@ -6,6 +6,11 @@ def index(request):
     return render(request, 'testApp/index.html')
 
 def detail(request, car_id):
-    latest_question_list = Cars.objects.filter(id=car_id)
-    context = {'CarInfo': latest_question_list}
+    carInfo = Cars.objects.filter(id=car_id)
+    context = {'CarInfo': carInfo}
     return render(request, 'testApp/carDetails.html', context)
+
+def results(request):
+    resultantCars = Cars.objects.filter(car_makename="LAND ROVER")
+    context = {'resultantCars': resultantCars}
+    return render(request, 'testApp/searchResults.html', context)
