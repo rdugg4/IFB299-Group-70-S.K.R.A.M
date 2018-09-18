b4e046e272b872c63a2b624e417acef81bf62796
kklimbasiya0005 <42435817+kklimbasiya0005@users.noreply.github.com>
2018-09-18 12:08:04 +1000

    changes

--- SKRAM70/testApp/urls.py
+++ SKRAM70/testApp/urls.py
@@ -6,10 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:car_id>/', views.detail, name='detail'),
-<<<<<<< HEAD
     path('searchResults', views.results, name='something'),
     path('create_account/<int:customer_id>/', views.accounts, name='Accounts')
-=======
     url(r'^search/$', views.search),
->>>>>>> a80a47f5d45ca643b4e9a45349f05e23077ef23d
 ]
