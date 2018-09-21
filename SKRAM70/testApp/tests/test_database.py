f7488499cf61d7bb066dda4254cbff91fa87de13
kklimbasiya0005 <42435817+kklimbasiya0005@users.noreply.github.com>
2018-09-21 22:06:38 +1000

    test

--- SKRAM70/testApp/tests/test_database.py
+++ SKRAM70/testApp/tests/test_database.py
@@ -6,3 +6,16 @@ class test_createAccountView(TestCase):
     def test_indexHTML(self):
         response = self.client.get('/CRC/create_account/')
         self.assertTemplateUsed(response, 'testApp/ShaleenCreateYourAccountPage.html')
+        
+class test_customerDetailView(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Customers.objects.create(firstname='Kaushal', middlename='Kishorbhai', lastname='Limbasiya',
+        tel = 1234567890,
+        bday = 09/09/1997,
+        email = 'abcd@gmail.com',
+        Password = 'abcd1234')
+        
+    def test_Location(self):
+        response = self.client.get('/CRC/create_account/')
+        self.assertEqual(response.status_code, 200)
