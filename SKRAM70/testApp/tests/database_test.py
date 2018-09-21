c7f22b3fd205bb20dc0b0111cc44183b5e843632
kklimbasiya0005 <42435817+kklimbasiya0005@users.noreply.github.com>
2018-09-21 21:31:59 +1000

    changes for testing

--- SKRAM70/testApp/tests/database_test.py
+++ SKRAM70/testApp/tests/database_test.py
@@ -4,5 +4,5 @@ from django.urls import reverse
 class test_createAccountView(TestCase):
     
     def test_indexHTML(self):
-        response = self.client.get('/CRC/create_account/')
+        response = self.client.get('/CRC/create_accounts/')
         self.assertTemplateUsed(response, 'testApp/ShaleenCreateYourAccountPage.html')
