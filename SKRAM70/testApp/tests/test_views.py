82e00d4705d284873dea6534db49ef5c2d33604f
rdugg4 <rdugg4@users.noreply.github.com>
2018-10-16 15:27:17 +1000

    Test Cases update

--- SKRAM70/testApp/tests/test_views.py
+++ SKRAM70/testApp/tests/test_views.py
@@ -1,15 +1,17 @@
 from django.test import TestCase
 from django.urls import reverse
 from testApp.models import *
+from django.contrib.auth.models import User, Group
+from django.contrib.messages import get_messages
 
 class test_indexView(TestCase):
 
     def test_Location(self):
-        response = self.client.get('/CRC/')
+        response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
     def test_Template(self):
-        response = self.client.get('/CRC/')
+        response = self.client.get('/')
         self.assertTemplateUsed(response, 'testApp/AlanaCustomerHomepage.html')
 
 class test_carDetailView(TestCase):
@@ -29,23 +31,22 @@ class test_carDetailView(TestCase):
         car_wheelbase = '4000mm')
 
     def test_Location(self):
-        response = self.client.get('/CRC/1/')
+        response = self.client.get('/CarInfo/1/')
         self.assertEqual(response.status_code, 200)
 
     def test_Template(self):
-        response = self.client.get('/CRC/1/')
+        response = self.client.get('/CarInfo/1/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'testApp/showcaroriginal.html')
 
     def test_Context(self):
-        carDBs = Cars.objects.filter(id=1)
-        response = self.client.get('/CRC/1/')
+        carDB = Cars.objects.get(id=1)
+        response = self.client.get('/CarInfo/1/')
         self.assertEqual(response.status_code, 200)
         self.assertTrue('CarInfo' in response.context)
         self.assertEqual(response.context['CarInfo'].count(), 1)
         for car in response.context['CarInfo']:
-            for carDB in carDBs:
-                self.assertEqual(car.id, carDB.id)
+            self.assertEqual(car.id, carDB.id)
 
 class test_searchResultsView(TestCase):
     @classmethod
@@ -68,10 +69,91 @@ class test_searchResultsView(TestCase):
             car_wheelbase = '4000mm {carid}')
 
     def test_Location(self):
-        response = self.client.get('/CRC/search/')
+        response = self.client.get('/search/')
         self.assertEqual(response.status_code, 200)
 
     def test_Template(self):
-        response = self.client.get('/CRC/search/')
+        response = self.client.get('/search/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'testApp/ShaleenSearchresults.html')
+
+
+class test_StaffPortalView(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        staffUser = User.objects.create_user('testStaff', 'test@email.com', 'testStaff')
+        staff_group, created = Group.objects.get_or_create(name='staff_group')
+        staff_group.user_set.add(staffUser)
+
+        Stores.objects.create(name = "testName",
+            address = "testAdress",
+            phone = "045555555",
+            city = "testCity",
+            state = "testState")
+
+        for storeObject in Stores.objects.all():
+            StaffMembers.objects.create(user=staffUser, storeid=storeObject)
+
+        BMUser = User.objects.create_user('testBM', 'test@email.com', 'testBM')
+        boardMember_group, created = Group.objects.get_or_create(name='boardMember_group')
+        boardMember_group.user_set.add(BMUser)
+
+    def test_NotLoggedIn(self):
+        response = self.client.post('/staffPortal')
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), 'You MUST be logged in to access that page')
+        self.assertRedirects(response, '/accounts/login/', status_code=302, target_status_code=200, msg_prefix='', fetch_redirect_response=True)
+
+    def test_LoggedInCustomer(self):
+        response = self.client.post('/staffPortal')
+
+    def test_LoggedInStaff(self):
+        self.client.login(username="testStaff", password="testStaff")
+        response = self.client.get('/staffPortal')
+        self.assertEqual(response.status_code, 200)
+
+    def test_LoggedInBM(self):
+        self.client.login(username="testBM", password="testBM")
+        response = self.client.get('/staffPortal')
+        self.assertEqual(response.status_code, 200)
+
+    def test_Template(self):
+        self.client.login(username="testStaff", password="testStaff")
+        response = self.client.get('/staffPortal')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'testApp/MikeStaffHomePage.html')
+
+class test_FAQView(TestCase):
+    def test_Location(self):
+        response = self.client.get('/FrequentlyAskedQuestions')
+        self.assertEqual(response.status_code, 200)
+
+    def test_Template(self):
+        response = self.client.get('/FrequentlyAskedQuestions')
+        self.assertTemplateUsed(response, 'testApp/FAQpage.html')
+
+class test_CarPopularityView(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        BMUser = User.objects.create_user('testBM', 'test@email.com', 'testBM')
+        boardMember_group, created = Group.objects.get_or_create(name='boardMember_group')
+        boardMember_group.user_set.add(BMUser)
+
+    def test_NotLoggedIn(self):
+        response = self.client.post('/staffPortal/CarPopularity/')
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]), 'You MUST be logged in to access that page')
+        self.assertRedirects(response, '/accounts/login/', status_code=302, target_status_code=200, msg_prefix='', fetch_redirect_response=True)
+
+    def test_LoggedInBM(self):
+        self.client.login(username="testBM", password="testBM")
+        response = self.client.get('/staffPortal/CarPopularity/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_Template(self):
+        self.client.login(username="testBM", password="testBM")
+        response = self.client.get('/staffPortal/CarPopularity/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'testApp/testCarpopularity.html')
